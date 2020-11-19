d41ac789200bda81b5b1ca1c67c92e2e85a1a4a6
MGL1994 <michaelgl1994@gmail.com>
2020-11-19 09:50:26 +0000

    Removed sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,8 @@ const config = {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|gif|pdf)$/,
