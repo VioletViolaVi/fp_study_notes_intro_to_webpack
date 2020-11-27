dcd41ee7bb9b19016479ae97f78d0d29bbd9c5b0
MGL1994 <michaelgl1994@gmail.com>
2020-11-27 12:35:13 +0000

    Added webpack production update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import TrainerCard from './components/TrainerCard';
+import PokemonCard from './components/PokemonCard';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    render() {
-        var helloWorld = 'Hello World!'
-      return (
-          <h1>{helloWorld}</h1>
-      )
-    }
-  }
+import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
